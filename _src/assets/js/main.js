01dfd48a98124d490447674969925cbbcf566c05
Elvira Fuente <hebebicho@gmail.com>
2019-04-22 15:39:19 +0200

    add js for button and get input value

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('>> Ready :)');
+//traer elementos html
+const inputEl = document.querySelector('#show');
+const buttonEl = document.querySelector('.button');
+
+function searchHandler(){
+  const inputShow = inputEl.value;
+  console.log(inputShow);
+}
+
+//listener
+buttonEl.addEventListener('click', searchHandler);
