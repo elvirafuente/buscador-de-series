26eb119f0a2bbd92d4746c39bb63823179f4a477
Elvira Fuente <hebebicho@gmail.com>
2019-04-22 17:06:56 +0200

    add placeholder for no img

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,6 +3,8 @@
 //traer elementos html
 const inputEl = document.querySelector('#show');
 const buttonEl = document.querySelector('.button');
+const resultList = document.querySelector('.result-list');
+
 
 function searchHandler(){
   const inputShow = inputEl.value;
@@ -13,7 +15,21 @@ function searchHandler(){
     .then(function(data){ 
       const seriesList = data;
       for (let i = 0; i < seriesList.length; i++){
-        console.log(`serie name: ${seriesList[i].show.name}`);
+        const showName = seriesList[i].show.name;
+        const newShow = document.createElement('li');
+        const newShowTitle = document.createElement('h3');
+        const newShowTitleContent = document.createTextNode(showName);
+        const newShowImg = document.createElement('img');
+        resultList.appendChild(newShow);
+        newShow.appendChild(newShowTitle);
+        newShowTitle.appendChild(newShowTitleContent);
+        newShow.appendChild(newShowImg);
+        if(!seriesList[i].show.image){
+          newShowImg.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        } else {
+          const showImgUrl = seriesList[i].show.image.medium;
+          newShowImg.src = showImgUrl;
+        }
       }
     });
 
